d38353477defad6ebdc4ba807b7f38f36bce17f5
kevin <d9pubg@gmail.com>
2025-11-03 08:10:49 +0700

    chore(dependencies): add spatie/laravel-permission package and update composer.lock

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create roles
+        $roles = ['admin', 'tutor', 'student'];
+        
+        foreach ($roles as $role) {
+            Role::create(['name' => $role]);
+        }
+    }
+}
