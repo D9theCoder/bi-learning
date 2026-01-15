0e9d6f84987df0e4359f8ec1df6c014bb6b40868
kevin <d9pubg@gmail.com>
2026-01-15 15:22:49 +0700

    build: Enforce HTTPS scheme in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Gate::before(function (User $user) {
             if ($user->hasRole('admin')) {
                 return true;
